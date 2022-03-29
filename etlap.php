e0ee7e611a13ed3e9378533d4a447f06d99fc379
Koza Gábor <97710592+Koza-Gabor@users.noreply.github.com>
2022-03-29 23:53:32 +0200

    Add files via upload

--- etlap.php
+++ etlap.php
@@ -16,3 +16,28 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $data->id = $pdo->lastInsertId();
     return;
 }
+
+if ($_SERVER['REQUEST_METHOD'] == 'PATCH') {
+    $data = json_decode(file_get_contents('php://input'));
+    //var_dump ($data);
+    //$sql = "UPDATE etlap SET category=?, name=?, osszetetel=?, alergia=?, price=? WHERE id=?";
+    //prepared_query($con, $sql, [$category, $name, $osszetetel, $alergia, $price, $id]);
+    //$stmt = $pdo->prepare($sql);
+    //$stmt->bind_param("sssssi", $category, $name, $osszetetel, $alergia, $price, $id);
+    $query = "UPDATE etlap SET category='$data->category', name='$data->name', osszetetel='$data->osszetetel', alergia='$data->alergia', price='$data->price' WHERE id=".$data->id;
+    //$stmt->execute();
+    $stmt = $pdo->query($query);
+    //$data->id = $pdo->stmt($stmt);
+    return;
+}
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    $data = json_decode(file_get_contents('php://input'));
+    $query = "DELETE FROM etlap WHERE id =".$data->id;
+    //$stmt->execute([.$data->id]);
+    $stmt =$pdo->query($query);
+    return;
+    
+}
+?>
